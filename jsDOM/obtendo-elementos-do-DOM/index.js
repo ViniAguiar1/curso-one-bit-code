9b29dd8bd40a183ffd4d8f0069e4466b23d75fd5
ViniAguiar1 <vinicius.aguiar1@icloud.com>
2023-04-12 11:41:06 -0300

    Att

--- jsDOM/obtendo-elementos-do-DOM/index.js
+++ jsDOM/obtendo-elementos-do-DOM/index.js
@@ -1,3 +1,14 @@
 function show() {
-    
+    const contactList = document.getElementById('contact-list')
+    console.log(contactList)
+
+    const listElements = document.getElementsByTagName('li')
+    console.log(listElements)
+
+    const contactInputs = document.getElementsByClassName('contact-input')
+    console.log(contactInputs)
+
+    const contacts = document.querySelectorAll('#contact-list > li > label')
+    console.log(contacts)
+
 }
