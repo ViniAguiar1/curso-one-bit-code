f393222c322bbae711e14b4a57266fd840f389bb
ViniAguiar1 <vinicius.aguiar1@icloud.com>
2023-04-18 18:37:01 -0300

    Att

--- jsDOM/manipulando-atributos/script.js
+++ jsDOM/manipulando-atributos/script.js
@@ -1,5 +1,27 @@
 const input = document.getElementById('input')
 
 document.getElementById('value').addEventListener('click', function() {
-    input.value = input.value === '' ? "Olá, mundo!" : ''
+    input.value = "Olá, mundo!"
+    // input.getAttribute('value')
+
+    console.log(input.value)
+    console.log(input.getAttribute('value'))
+})
+
+document.getElementById('type').addEventListener('click', function(){
+    // input.type = input.type !== 'date' ? 'date' : 'text'
+    input.setAttribute('type', 'radio')
+})
+
+document.getElementById('placeholder').addEventListener('click', function(){
+    input.placeholder = 'Digite Algo...'
+})
+
+document.getElementById('disable').addEventListener('click', function() {
+    input.setAttribute('disable', !input.disabled)
+})
+
+document.getElementById('data').addEventListener('click', function(){
+    const data = input.dataset.something
+    console.log(`O valor do atributo data-something é: ${data}`)
 })
