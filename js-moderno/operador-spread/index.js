3a462872d0377842ad99c1b14f12ead4baee41e4
ViniAguiar1 <vinicius.aguiar1@icloud.com>
2023-04-23 20:22:36 -0300

    Att

--- js-moderno/operador-spread/index.js
+++ js-moderno/operador-spread/index.js
@@ -3,3 +3,7 @@ console.log(...name) // separa as letras "V i n i c i u s"
 
 let arr = [4, 2, 3, 1, 20]
 console.log(...arr) // separa os elementos do array
+
+const towns = ['Prontera', 'Izlude', 'Payon', 'Alberta', 'Geffen', 'Morroc']
+
+console.log(towns)
