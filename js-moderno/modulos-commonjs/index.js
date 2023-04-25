488fd238a867bba5db55b607a140fcdfeb3bfe0a
ViniAguiar1 <vinicius.aguiar1@icloud.com>
2023-04-25 18:44:05 -0300

    Att

---
+++ js-moderno/modulos-commonjs/index.js
@@ -0,0 +1,11 @@
+function render() {
+    console.log('Renderizando a interface da aplicação...')
+}
+function store(){
+    console.log("Salvando as informações no banco de dados...")
+}
+
+console.log("Aplicação inicializada...")
+render()
+store()
+console.log("Aplicação finalizada...")
