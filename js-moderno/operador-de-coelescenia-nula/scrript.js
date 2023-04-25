9c5607611bd46532a1069e2cb6253a810e223816
ViniAguiar1 <vinicius.aguiar1@icloud.com>
2023-04-25 11:45:07 -0300

    Att

---
+++ js-moderno/operador-de-coelescenia-nula/scrript.js
@@ -0,0 +1,26 @@
+// // const a = 0 // false
+
+// // const b = null
+
+// // const c = "teste"
+// // console.log(a || b || c)
+// // console.log(a ?? b ?? c)
+
+// let a = 0
+// let b = a || 42
+// let c = null
+// console.log({ a, b})
+
+// b = c ?? 55
+// console.log({ c, b})
+
+let person = {
+    name: "John Doe",
+    email: "doejohn@email.com",
+    birthday: "1990-03-14",
+    age: 32
+    }
+    
+    let x = 0
+    x = person.data.age
+    console.log(x)
