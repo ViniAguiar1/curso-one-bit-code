3ff1414d1773ed8f185ae3edfbb0aac8ae6e94e5
ViniAguiar1 <vinicius.aguiar1@icloud.com>
2023-05-03 13:05:12 -0300

    Att

---
+++ poo/classes-do-es6/Book.js
@@ -0,0 +1,12 @@
+class Book {
+    constructor(title){
+        // instaciar = ato de criar uma instacia
+        this.title = title
+    }
+}
+
+const eragon = new Book("Eragon")
+const eldest = new Book("Eldest")
+
+console.log(eragon)
+console.log(eldest)
